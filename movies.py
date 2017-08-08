4b1fac30a601b6fed8e5f1a1043f478e0319de6d
Abdul Rasheed <narejo@gmail.com>
2017-08-08 15:48:07 +1000

    adjust Movie class PEP8 recommendations

--- movies.py
+++ movies.py
@@ -1,5 +1,6 @@
 class Movie():
-    def __init__(self, title, year_of_release, imdb_rating, poster_url, trailer_url):
+    def __init__(self, title, year_of_release,
+                imdb_rating, poster_url, trailer_url):
         self.title = title
         self.year_of_release = year_of_release
         self.imdb_rating = imdb_rating
